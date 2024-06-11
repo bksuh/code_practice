d8c1f982dfe7980acfa0686171c1ad6755d6f760
bksuh <162313703+bksuh@users.noreply.github.com>
2024-06-11 11:51:24 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,40 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr=[]
+
+for _ in range(n):
+    tmp = tuple(sys.stdin.readline().split())
+    if tmp[0] == 'push':
+        arr.append(int(tmp[1]))
+    
+    elif tmp[0] == 'front':
+        if len(arr) !=0:
+            print(arr[0])
+        else:
+            print(-1)
+    
+    elif tmp[0] == 'back':
+        if len(arr) != 0:
+            print(arr[-1])
+        else:
+            print(-1)
+    
+    elif tmp[0] == 'empty':
+        if len(arr) ==  0:
+            print(1)
+        else:
+            print(0)
+
+    elif tmp[0] == 'size':
+        print(len(arr))
+    
+    elif tmp[0] == 'pop':
+        if len(arr) != 0:
+            tmp1 = list(reversed(arr))
+            print(tmp1.pop())
+            arr = list(reversed(tmp1))
+
+        else:
+            print(-1)
+
