c9b22186d4900a6758a9f7dcfa159f3166054996
bksuh <162313703+bksuh@users.noreply.github.com>
2024-06-22 20:16:36 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+for i in range(1,n+1):
+    print("*"* i, end ='')
+    print(' '* 2*(n-i), end ='')
+    print('*' * i)
+for i in range(1, n):
+    print("*"*(n-i), end='')
+    print(' '*2*i, end ='')
+    print("*"*(n-i))
+    
