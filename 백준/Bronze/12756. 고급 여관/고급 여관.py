235a146b1ad9ea49df3febfe83b2201114f8bfa8
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-07-10 00:51:48 +0900

    [Bronze III] Title: 고급 여관, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/12756. 고급 여관/고급 여관.py
@@ -0,0 +1,16 @@
+a1, a2 = map(int, input().split())
+b1, b2 = map(int, input().split())
+
+while True:
+    a2 = a2 - b1
+    b2 = b2 - a1
+
+    if a2<=0 and b2<=0:
+        print("DRAW")
+        break
+    elif a2>0 and b2<=0:
+        print("PLAYER A")
+        break
+    elif b2>0 and a2<=0:
+        print("PLAYER B")
+        break
