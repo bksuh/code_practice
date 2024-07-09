54a760a3e1771cd0827a81f13858328158519e98
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-07-10 00:45:35 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
@@ -0,0 +1,11 @@
+while True:
+    a, b, c, d = map(int, input().split())
+    if a==0 and b==0 and c==0 and d==0:
+        break
+    cnt = 0
+    while True:
+        if a==b and b==c and c==d and d==a:
+            break
+        a, b, c, d = abs(a-b), abs(b-c), abs(c-d), abs(d-a)
+        cnt+=1
+    print(cnt)
