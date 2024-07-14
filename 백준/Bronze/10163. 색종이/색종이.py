d51401546662c4eac6ec21f85f8922766fed3a21
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-07-14 13:43:06 +0900

    [Bronze I] Title: 색종이, Time: 1796 ms, Memory: 39120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,15 +1,16 @@
-grid = [[0]*1001 for _ in range(1001)]
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-for i in range(1, n+1):
-    a, b, c, d = map(int, input().split())
-    for x in range(a, a+c):
-        for y in range(b, b+d):
-            grid[x][y] = i
-ans = [0 for _ in range(n+1)]
-for i in range(len(grid)):
-    for k in range(1, n+1):
-        ans[k] +=grid[i].count(k)
+grid = [[0 for _ in range(1001)] for _ in range(1001)]
+num_of_papers = int(input())
 
-for i in range(1, len(ans)):
-    print(ans[i])
+for p in range(1, num_of_papers+1):
+    p_x, p_y, width, height = map(int, input().split())
+    for y in range(p_y, p_y+height):
+        grid[y][p_x:(p_x+width)] = [p]*width
+            
+for p in range(1, num_of_papers+1):
+    result = 0
+    for cnt in range(1001):
+        result += grid[cnt].count(p)
+    print(result)
