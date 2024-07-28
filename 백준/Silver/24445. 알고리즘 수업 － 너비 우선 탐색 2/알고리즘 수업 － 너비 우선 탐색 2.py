1ea083e87ae17c62baaff511cf10c10c272017c8
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-07-28 17:20:10 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 648 ms, Memory: 58988 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,32 @@
+import collections
+import sys
+
+input = sys.stdin.readline
+
+n, k, s = map(int, input().split())
+graph = [[]for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+for _ in range(k):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for k in range(len(graph)):
+    graph[k].sort(reverse=True)
+def bfs(graph, visited, s):
+    queue = collections.deque()
+    queue.append(s)
+    cnt = 1
+    visited[s] = cnt
+    while queue:
+        current = queue.popleft()
+        for k in graph[current]:
+            
+            if not visited[k]:
+                cnt+=1
+                visited[k] = cnt
+                queue.append(k)
+bfs(graph, visited, s)
+
+for i in range(1, len(visited)):
+    print(visited[i])
