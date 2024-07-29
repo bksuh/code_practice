cc65232df157f6d52ae22fabdbc5193cbabc3e49
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-07-29 16:58:44 +0900

    [Gold III] Title: 불!, Time: 2288 ms, Memory: 42552 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.py
@@ -0,0 +1,45 @@
+import collections
+dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+r, c = map(int, input().split())
+graph = []
+queue = collections.deque()
+
+for i in range(r):
+    tmp = list(input())
+    for j in range(len(tmp)):
+        if tmp[j] == 'J':
+            sx, sy = (i, j)
+        elif tmp[j]=='F':
+            queue.append((i, j))
+    graph.append(tmp)
+graph[sx][sy] = 1
+queue.append((sx, sy))
+while queue:
+    x, y = queue.popleft()
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            continue
+        if graph[nx][ny] == '.':
+            if graph[x][y] == 'F':
+                graph[nx][ny] = 'F'
+            elif isinstance(graph[x][y], int):
+                graph[nx][ny] = graph[x][y] + 1
+            queue.append((nx, ny))
+        else:
+            continue
+
+ans = 100000000
+for i in range(r):
+    for j in range(c):
+        
+        if graph[i][j] == '#' or graph[i][j] == 'F' or graph[i][j] == '.':
+            continue
+        elif i == 0 or i == r-1 or j == 0 or j == c-1:
+            ans = min(ans, graph[i][j])
+
+if ans == 100000000:
+    print("IMPOSSIBLE")
+else:
+    print(ans)
