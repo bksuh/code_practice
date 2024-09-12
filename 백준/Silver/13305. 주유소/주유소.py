b5b00d17577e81cb1084426d2611e5e36dd5db36
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-09-12 18:52:19 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,19 +1,13 @@
-n = int(input())
-dist = list(map(int, input().split()))
-gas = list(map(int, input().split()))
+n=int(input())
+km=list(map(int,input().split()))
+price=list(map(int,input().split()))
 
-import sys
-tmp = sys.maxsize
-result = [tmp]*n
-result[0] = 0
+minPrice=price[0]
+total=0
 
-for i in range (len(gas)-1):
-    current_price = gas[i]
-    for j in range(len(dist)):
-        distance_target = sum(dist[i:j+1])
-        if distance_target == 0:
-            continue
-        else:
-            tmp = current_price * distance_target
-            result[j+1] = min(result[j+1], result[i]+ tmp)
-print(result[-1])
+for i in range(n-1):
+    if minPrice>price[i]:
+        minPrice=price[i]
+
+    total+=(minPrice*km[i])
+print(total)
