4a8d18de8c656c26d560847a346f17bf01a8db1f
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2024-11-07 17:37:49 +0900

    [Silver II] Title: Sheba’s Amoebas, Time: 60 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/15092. Sheba’s Amoebas/Sheba’s Amoebas.py
@@ -0,0 +1,34 @@
+import collections
+dx = [1, 1, 0, -1, -1, -1, 0, 1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]
+
+def bfs(sx, sy, grid):
+    queue = collections.deque()
+    grid[sx][sy] = 0
+    queue.append((sx, sy))
+    while queue:
+        sx, sy = queue.popleft()
+        for k in range(len(dx)):
+            nx = sx + dx[k]
+            ny = sy + dy[k]
+            if nx < 0 or nx >= n or ny< 0 or ny>=m:
+                continue
+            if grid[nx][ny] == '#':
+                grid[nx][ny] = '.'
+                queue.append((nx, ny))
+                
+    return grid
+
+n, m = map(int, input().split())
+
+grid =[]
+cnt = 0
+for _ in range(n):
+    tmp = [c for c in input()]
+    grid.append(tmp)
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == '#':
+            grid = bfs(i, j, grid)
+            cnt += 1
+print(cnt)
