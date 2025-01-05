7573aaafa3c6656648b23495b76ae05434bcdb4d
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2025-01-05 10:00:29 +0900

    [level 0] Title: 커피 심부름, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    for drink in order:
+        if 'americano' in drink:
+            answer += 4500
+        elif 'cafelatte' in drink:
+            answer += 5000
+        else:
+            answer += 4500
+    return answer
