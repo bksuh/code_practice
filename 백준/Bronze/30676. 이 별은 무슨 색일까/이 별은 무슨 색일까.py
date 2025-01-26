f65c1ec27d4878c4352030e8c6b19b04ab76ba24
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2025-01-26 10:14:59 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+height = int(input())
+
+if 380 <= height < 425:
+    print('Violet')
+elif height < 450:
+    print('Indigo')
+elif height < 495:
+    print('Blue')
+elif height < 570:
+    print('Green')
+elif height < 590:
+    print('Yellow')
+elif height < 620:
+    print('Orange')
+elif height <= 780:
+    print('Red')
